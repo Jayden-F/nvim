3e1c98acf5c45844f3f9a8539b45d76d0801e3d8
Jayden Filippi <jaydenjfilippi@gmail.com>
2023-12-14 12:40:04 +1000

    personalised bindings

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,5 +1,12 @@
 require('refactoring').setup({})
 
-vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
+-- vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
+-- load refactoring Telescope extension
+require("telescope").load_extension("refactoring")
 
+vim.keymap.set(
+	{"n", "x"},
+	"<leader>rr",
+	function() require('telescope').extensions.refactoring.refactors() end
+)
 
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,9 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pgf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
+
 vim.keymap.set('n', '<leader>gt', builtin.tags,            { desc = '[G]o to C[T]ags (telescope)', noremap = true })
 
 function RTags()

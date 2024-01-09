33849e81fee92cea15873c47ee1fd1bd4053998e
jayden <jaydenjfilippi@gmail.com>
2024-01-09 14:43:06 +1100

    add nerd tree commenter

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -78,4 +78,5 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use 'preservim/nerdcommenter'
 end)

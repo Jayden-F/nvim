05f0a5b9da43af758b72abd1c31d0d30d361b4c3
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-03-18 11:23:12 +1100

    add unicode tree for neogit and update telescope.

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,15 +1,13 @@
 local neogit = require('neogit')
 
-
-
 neogit.setup {
     -- Place your configuration parameters for neogit here.
     -- For example:
     integrations = {
-        diffview = true,     -- Enables the diffview integration if you're using diffview
+        diffview = true, -- Enables the diffview integration if you're using diffview
         telescope = true,
-    }
+    },
+    graph_style = "unicode",
 }
-
 
 vim.keymap.set("n", "<leader>gs", function() neogit.open() end)

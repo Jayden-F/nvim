9aeead43ab91c3c11a386781d14399b05abc88ec
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-04-03 17:42:20 +1100

    add telescope undo.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,4 @@
-return {
+return { {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.6',
     -- or                              , branch = '0.1.x',
@@ -20,5 +20,39 @@ return {
 
         { "<leader>gt", function() require('telescope.builtin').tags() end,                             "go to ctags" },
         { "<leader>rt", function() vim.cmd [[!ctags -f .tags --languages=all --exclude=.git -R .]] end, "refresh ctags" },
+    },
+},
+    {
+        "debugloop/telescope-undo.nvim",
+        event = "BufReadPost",
+        dependencies = { -- note how they're inverted to above example
+            {
+                "nvim-telescope/telescope.nvim",
+                dependencies = { "nvim-lua/plenary.nvim" },
+            },
+        },
+        keys = {
+            { -- lazy style key map
+                "<leader>u",
+                "<cmd>Telescope undo<cr>",
+                desc = "undo history",
+            },
+        },
+        opts = {
+            -- don't use `defaults = { }` here, do this in the main telescope spec
+            extensions = {
+                undo = {
+                    -- telescope-undo.nvim config, see below
+                },
+                -- no other extensions here, they can have their own spec too
+            },
+        },
+        config = function(_, opts)
+            -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+            -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+            -- defaults, as well as each extension).
+            require("telescope").setup(opts)
+            require("telescope").load_extension("undo")
+        end,
     },
 }

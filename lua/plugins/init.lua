76b2061e25a11d79f44cd3ec1d02493c325e688d
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-04-03 17:14:24 +1100

    set up comment, tree sitter context and surround.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,13 @@
 return {
     'nvim-tree/nvim-web-devicons',
     "mbbill/undotree",
-    "kylechui/nvim-surround",
-    {"nvim-treesitter/nvim-treesitter-context", dependencies = {"nvim-treesitter/nvim-treesitter",}}
+    {
+        "kylechui/nvim-surround",
+        lazy = false,
+        config = function()
+            require("nvim-surround").setup({})
+        end
+    },
+    { 'numToStr/Comment.nvim',                   opts = {},                                            lazy = false, },
+
 }

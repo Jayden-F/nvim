2270fef1e1724575a9278bcf67ca3780191b7678
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-04-03 22:00:14 +1100

    add nvim hardtime and fix plugins.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,5 +8,10 @@ return {
         end
     },
     { 'numToStr/Comment.nvim', opts = {}, event = "BufReadPost", },
-
+    {
+        "m4xshen/hardtime.nvim",
+        event = "BufReadPost",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {}
+    },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,17 +2,9 @@ return { {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "BufReadPost",
-
     opts = {
         highlight = {
-            enable = false,
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
+            enable = true,
             additional_vim_regex_highlighting = false,
         },
         autopairs = { enable = true },

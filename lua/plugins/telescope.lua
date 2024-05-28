83d02fa56f8f7bdc0ab060749f45efe1c410e00f
Jayden Filippi <jaydenjfilippi@gmail.com>
2024-05-28 10:51:20 +1000

    versioning: change neogit and telescope versions.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return { {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim', },
     keys = {
         { "<leader>ff", function() require('telescope.builtin').find_files() end,                       "find files" },

b5b79eeb4691fa97e27cac559e7e15e5b9d87ddc
jayden filippi <jaydenjfilippi@gmail.com>
2024-07-04 11:19:59 +1000

    add git signs

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,13 +1,21 @@
 return {
-    "NeogitOrg/neogit",
-    branch = "master",
-    dependencies = {
-        "nvim-lua/plenary.nvim",  -- required
-        "sindrets/diffview.nvim", -- optional - Diff integration
-
-        -- Only one of these is needed, not both.
-        "nvim-telescope/telescope.nvim", -- optional
+    {
+        "NeogitOrg/neogit",
+        branch = "master",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        keys = { { "<leader>gs", function() require('neogit').open() end, "Neogit" } },
+        opts = { graph_style = "unicode", }
     },
-    keys = { { "<leader>gs", function() require('neogit').open() end, "Neogit" } },
-    opts = { graph_style = "unicode", }
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = function()
+            require('gitsigns').setup()
+        end,
+    },
 }

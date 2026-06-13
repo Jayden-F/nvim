6df3264aca09f2e6d41191846baeacd0cd2c38b4
jayden filippi <jaydenjfilippi@gmail.com>
2026-06-13 13:10:19 +1000

    Fix

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -30,51 +30,6 @@ vim.api.nvim_create_autocmd("User", {
     end,
 })
 
----@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
-local progress = vim.defaulttable()
-vim.api.nvim_create_autocmd("LspProgress", {
-    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        local value = ev.data.params
-            .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
-        if not client or type(value) ~= "table" then
-            return
-        end
-        local p = progress[client.id]
-
-        for i = 1, #p + 1 do
-            if i == #p + 1 or p[i].token == ev.data.params.token then
-                p[i] = {
-                    token = ev.data.params.token,
-                    msg = ("[%3d%%] %s%s"):format(
-                        value.kind == "end" and 100 or value.percentage or 100,
-                        value.title or "",
-                        value.message and (" **%s**"):format(value.message) or ""
-                    ),
-                    done = value.kind == "end",
-                }
-                break
-            end
-        end
-
-        local msg = {} ---@type string[]
-        progress[client.id] = vim.tbl_filter(function(v)
-            return table.insert(msg, v.msg) or not v.done
-        end, p)
-
-        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-        vim.notify(table.concat(msg, "\n"), "info", {
-            id = "lsp_progress",
-            title = client.name,
-            opts = function(notif)
-                notif.icon = #progress[client.id] == 0 and " "
-                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-            end,
-        })
-    end,
-})
-
 vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
@@ -82,10 +37,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, opts)
     end,
